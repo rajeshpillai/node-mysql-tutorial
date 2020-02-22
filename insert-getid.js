405a7d02189f0c5e2a4b8a91a7b9836d34da8698
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-22 23:21:27 +0530

    5. Insert a row and get back the inserted row's id

---
+++ insert-getid.js
@@ -0,0 +1,18 @@
+let mysql = require('mysql');
+let config = require('./config.js');
+let connection = mysql.createConnection(config);
+
+let stmt = `INSERT INTO todos(title,completed)
+            VALUES(?,?)`;
+let todo = ['Insert a new row with placeholders', false];
+
+// execute the insert statment
+connection.query(stmt, todo, (err, results, fields) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  // get inserted id
+  console.log('Todo Id:' + results.insertId);
+});
+
+connection.end();
